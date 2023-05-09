566e3ed07e7a85c353ccca9c54d46808703c7519
hanh vo <hahnvo422@gmail.com>
2023-05-09 16:17:04 -0500

    Initial Commit

---
+++ NewsAppSwiftUI/NewsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppSwiftUIApp.swift
+//  NewsAppSwiftUI
+//
+//  Created by Hanh Vo on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
